2f161d0b617b50629d659e12397916f4bcd5ae51
Tyrease Teer <kayaneseera@gmail.com>
2021-05-09 14:29:13 -0700

    Added User Prompt
    We can now ask the user for ID and Password successfully.

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PankoojiManagementProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Login or create account, and pair keys
+            StartUp.Init();
+
+            //TODO: Selection Cycle (what to do inside the account)
+            if (StartUp.Success)
+                System.Begin();
+
+            Console.ReadLine();
+        }
+    }
+
+    internal static class StartUp
+    {
+        internal static bool Success { get; private set; }
+
+        internal static void Init()
+        {
+            //TODO: Ask for PanID
+            //TODO: Ask for Password
+            Console.WriteLine("");
+            string userIdentification = UserPrompt.Ask("PanID: ", true);
+            string userPassword = UserPrompt.Ask("\nPassword: ", true);
+
+            //TODO: Match PanID and Password to database, and check for valid key
+            Identification inputID = new Identification(userIdentification);
+            Password inputPassword = new Password(userPassword);
+
+            PanAccount validationAccount = PanAccount.New(inputID, inputPassword);
+
+            bool accountMatch = PanAuthorization.Validate(validationAccount);
+
+            //TODO: Add option to create new account.
+
+            Success = accountMatch;
+        }
+    }
+
+    //The program after successful login or account create and key creation
+    internal class System
+    {
+        internal static void Begin()
+        {
+            //Main Selection Loop begins here
+
+            //TODO: Create thread of main system
+
+        }
+    }
+
+    //User Account used to access list of passwords
+    internal class PanAccount
+    {
+        PanAccount(Identification id, Password password)
+        {
+            accountID = id;
+            accountPassword = password;
+        }
+
+        internal static PanAccount New(Identification id, Password password)
+        {
+            return new PanAccount(id, password);
+        }
+
+        internal Identification GetAccountId() => accountID;
+
+        internal Password GetAccountPassword() => accountPassword;
+
+        Identification accountID;
+        Password accountPassword;
+    }
+
+    //PanAccount Authorization Class
+    internal class PanAuthorization
+    {
+        internal static bool Successful { get; private set; }
+        internal static bool Validate(PanAccount targetPanAccount)
+        {
+            //TODO: Search through database for existing account, otherwise, ask to create a new one
+            //For now, fake access.
+
+            Successful = true;
+            return Successful;
+        }
+    }
+
+    //Identification of user PanAccount
+    internal class Identification
+    {
+        internal string ID { get; private set; }
+        internal Identification(string value)
+        {
+            ID = value;
+        }
+    }
+
+    //Password of user PanAccount
+    internal class Password
+    {
+        internal string Data { get; private set; }
+        internal Password(string value)
+        {
+            Data = value;
+        }
+    }
+
+    //Main connection to database/sql operations during main selection loop
+    internal class DatabaseSession
+    {
+
+    }
+
+    //Key used to match one device to database, or one external drive to device to database
+    internal class Key
+    {
+
+    }
+
+    //Encodes all infromation through unique algorithm
+    internal class SecureEncode
+    {
+
+    }
+
+
+    //Form Structure Class simply used to strucutre data input.
+    //Will parse and extract data based on user input
+    internal class ConsoleForm
+    {
+
+    }
+
+    internal class UserPrompt
+    {
+        internal static string Ask(string prompt)
+        {
+            return Ask(prompt, false);
+        }
+
+        internal static string Ask(string prompt, bool allowInterception)
+        {
+
+            Console.WriteLine(prompt);
+
+            //Check if to allow interception
+            if (allowInterception)
+            {
+                string input = string.Empty;
+                while (true)
+                {
+                    var key = Console.ReadKey(allowInterception);
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        return input;
+                    }
+                    input += key.KeyChar;
+                }
+            }
+
+            return Console.ReadLine();
+        }
+    }
+}
